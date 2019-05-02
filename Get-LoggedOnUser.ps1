adf9954b66e139fa211b555131acdbe9b577ad13
Tadhg O Briain <tadhg.obriain@live.com>
2019-05-02 16:54:53 +0100

    Collating disparate scripts

---
+++ Get-LoggedOnUser.ps1
@@ -0,0 +1,53 @@
+﻿Function Get-LoggedOnUser {
+	<#
+	.SYNOPSIS
+		This function queries CIM on the local or a remote computer and returns the user (local or Active Directory) that is currently
+		logged on.
+	
+	.EXAMPLE
+		PS> Get-LoggedOnUser
+	
+		This would query the local computer and return the user logged on.
+		
+	.EXAMPLE
+		PS> Get-LoggedOnUser -ComputerName CLIENT
+	
+		This would query the remote computer CLIENT and return the user logged on.
+	
+	.PARAMETER ComputerName
+		The name of the computer you'd like to run this function against.
+	
+	#>
+	[OutputType([pscustomobject])]
+	[CmdletBinding()]
+	Param (
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string[]]$ComputerName = $env:COMPUTERNAME
+        
+	)
+	Begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	Process {
+		Try {
+			foreach ($comp in $ComputerName)
+			{
+				$output = @{ 
+					ComputerName = $comp 
+					UserName = 'Unknown'
+					ComputerStatus = 'Offline'
+				}
+				if (Test-Connection -ComputerName $comp -Count 1 -Quiet) {
+					$output.UserName = (Get-CimInstance -Class win32_computersystem -ComputerName $comp).UserName
+					$output.ComputerStatus = 'Online'
+				}
+				[pscustomobject]$output
+			}
+		}
+		catch
+		{
+			$PSCmdlet.ThrowTerminatingError($_)
+		}
+	}
+}
---
+++ Send-ITTEmailAlert.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>Function Send-ITTEmailAlert {
+Param(
+[String] $SupportEmailAddress,
+[String] $MailingServer,
+[String] $EmailSubject,
+[String] $EmailBody,
+[String] $SmtpServer,
+[String] $EmailCc)
+
+Send-MailMessage -To $SupportEmailAddress -From $MailingServer `
+-Subject $EmailSubject -Body $EmailBody -SmtpServer $SmtpServer -Cc $EmailCc -Priority High
+
+}
