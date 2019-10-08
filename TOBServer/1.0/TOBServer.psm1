86e552b1c0f255ac8749ae375047c86f94876fb1
Tadhg O Briain <tadhg.obriain@live.com>
2019-10-08 15:52:10 +0100

    Major rewrite for TUDublin merger

--- TOBServer/1.0/TOBServer.psm1
+++ TOBServer/1.0/TOBServer.psm1
@@ -272,20 +272,16 @@ Function Get-TUDUser {
     #>
 
     [CmdletBinding(PositionalBinding=$False,
-                   ConfirmImpact='Medium')]
+                    ConfirmImpact='Medium')]
     [OutputType([String])]
 
     Param (
         # Param1 help description
         [Parameter(Mandatory=$True,
-                   Position=0,
-                   ValueFromPipeline=$True)]
+                    Position=0,
+                    ValueFromPipeline=$True)]
         [ValidatePattern('^x[0-9]{8}$')]
         [String[]]$Identity,
-
-        # Param2 help description
-        [Parameter(Mandatory=$False)]
-        [Switch]$Home,
 
         # Param3 help description
         [Parameter(Mandatory=$False)]
@@ -293,55 +289,47 @@ Function Get-TUDUser {
     )
 
     Begin {
-        Write-Output 'ToDo: Need an enabled core erasmus student account so as to tabulate results of multiple AD accounts properly.'
         Write-Output 'ToDo: Need to have $Identity as an output to pipe into Reset-ITTUser.'
 
         Set-ODPNetManagedDriver
 
-        $Domain = (Get-ADForest).Domains
+        $SearchDCs = @('compdc1.computing.stu.it-tallaght.ie','studc1.stu.it-tallaght.ie','engdc1.eng.stu.it-tallaght.ie')
 
         $TextInfo = (Get-Culture).TextInfo
 
-        $HomeFolderQuota = 4
-       
+        $Company = 'Tallaght'
+        $Office = 'Tallaght'
+        $ExtensionAttribute15 = 'TUDublin'
+        
         $COREconnString = "User id=itt_viewer;Password=""hug67*="" ;Connection Timeout=60;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=193.1.122.12)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=core22)))"
         $CompDBconnString ="User id=sys; Password=Xy9MEEtj; DBA Privilege=SYSDBA; Connection Timeout=60; Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.10.2.7)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=GLOBAL1))); Min Pool Size=10; Connection Lifetime=120; Connection Timeout=60; Incr Pool Size=5; Decr Pool Size=2; Max Pool Size=100; Validate Connection=True"
 
         # Create tables 
         $UserTable = New-Object System.Data.DataTable 'Results'
-        $CompDBUserTable = New-Object System.Data.DataTable 'CompDB Results'
-    
+
         # Define and add the columns
-        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'ValidUser',([String])))
-        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'Duplicate',([String])))
+        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'Domain',([String])))
         $UserTable.Columns.Add((New-Object System.Data.DataColumn 'ADAccount',([String])))
         $UserTable.Columns.Add((New-Object System.Data.DataColumn 'LastLogonDate',([String])))
         $UserTable.Columns.Add((New-Object System.Data.DataColumn 'PasswordExpired',([String])))
+        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'Office365 Valid Setup',([String])))
         $UserTable.Columns.Add((New-Object System.Data.DataColumn 'EmailAddress',([String])))
         $UserTable.Columns.Add((New-Object System.Data.DataColumn 'DataRetentionPolicy',([String])))
-
-        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'HomeFolder',([String])))
-        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'HomeFolderPermissions',([String])))
-        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'HomeFolderUsage',([String])))
-
-        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'DBAccount',([String])))
-        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'DBAccount_Status',([String])))
-        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'DBAccount_DefaultPwd',([String])))
 
         $Action = ''
     
         $ActionType = @{
-                'NoAction' = @'
+            'NoAction' = @'
 No action is necessary at this time.
 '@
-                'NotRegistered' = @'
+            'NotRegistered' = @'
 According to our records you are not currently registered.
 
 Please contact the Registrars/Part-Time/Finance office and clarify if there is a delay, or if there is something outstanding that they require from you.
 
 An account will be setup automatically once your registration is complete.
 '@
-                'AccountDisabled' = @'
+            'AccountDisabled' = @'
 According to our records there are outstanding issues with your registration.
 
 As a result of this your account has been automatically disabled.
@@ -361,7 +349,7 @@ Fulltime students should contact the Finance Department:
 
 If you are still have difficulties with your account following that, please contact us again if you require a password reset etc.
 '@
-                'NoBirthdate' = @'
+            'NoBirthdate' = @'
 According to our records there are outstanding issues with your registration.
 
 A birthdate is not recorded in our systems.
@@ -384,29 +372,10 @@ Fulltime students should contact the Finance Department:
 
 If you are still have difficulties with your account following that, please contact us again if you require a password reset etc.
 '@
-                'Duplicate' = @'
-You have a previous user account in another School/Department which needs to be deleted. 
-
-We have requested this happen as soon as possible. (to be coded!)
-
-Once this is done, you will be able to login to Moodle successfully.                
+            'Part-time EL student' = @'
+No AD user accounts are created for part-time students who are 'EL' registered.                
 '@
-                'HomeFolderFull' = @'
-The home folder associated with your student account is full or nearly full. 
-
-There is a quota applying to your account.
-
-Please consider either of the following:
-(1) Delete files that you no longer wish to keep
-(2) You alternative storage, e.g. Microsoft Office 365 (1TB storage)
-            
-            https://login.microsoftonline.com               
-'@
-                'HomeFolderPermissions' = @'
-The home folder associated with your student account had permission errors.
-We have fixed these errors so that your home folder and files should be accessible again.              
-'@
-               }
+        }
     }
     Process {
         Write-Output `n
@@ -415,104 +384,76 @@ We have fixed these errors so that your home folder and files should be accessib
             $Id = $TextInfo.ToTitleCase($Id)
 
             $COREsqlString = @" 
-                    SELECT ID, FIRSTNAME, LASTNAME, TO_CHAR(BIRTHDATE, 'DDMonYY') AS PASSWORD, YEARATT, PROGRAMME, REGCODE, ACCOUNT_ACTION, SUBSTR(TERM,1,4) AS TERM, LAST_UPDATED_DATE_TIME AS LAST_UPDATE
+                SELECT ID, FIRSTNAME, LASTNAME, TO_CHAR(BIRTHDATE, 'DDMonYY') AS PASSWORD, YEARATT, PROGRAMME, REGCODE, ACCOUNT_ACTION, SUBSTR(TERM,1,4) AS TERM, LAST_UPDATED_DATE_TIME AS LAST_UPDATE
 
-                    FROM ITT_STUDENT   
-                 
-					WHERE ID LIKE '$Id%'                    
+                FROM ITT_STUDENT   
+                
+                WHERE ID LIKE '$Id'                    
 "@
 
             $CompDBsqlString = @" 
-                                SELECT Username,Account_Status FROM dba_users WHERE username LIKE `'$Id`%`'
+                            SELECT Username,Account_Status FROM dba_users WHERE username LIKE `'$Id`'
 "@
-            
+                
             Get-DBResult  $COREconnString $COREsqlString $UserTable
-            
-            $RegUser = $UserTable.Rows | Where-Object ID -Like $ID*
+                
+            $RegUser = $UserTable.Rows | Where-Object ID -Like $ID
             If ($RegUser) { 
-                $FoundDomain = New-Object System.Collections.Generic.List[System.Object]
-                $DomADAccount = New-Object System.Collections.Generic.List[System.Object]
-                $DomLastLogonDate = New-Object System.Collections.Generic.List[System.Object]
-                $DomPasswordExpired = New-Object System.Collections.Generic.List[System.Object]
-                $DomEmailAddress = New-Object System.Collections.Generic.List[System.Object]
-                $DomDataRetentionPolicy = New-Object System.Collections.Generic.List[System.Object]
+                # Search all student domains till we find the AD user account
+                $DC = 0
+                Do {
+                    $ADUser = Get-ADUser -Filter "SamAccountName -Like '$ID'" -Properties Company,Office,EmailAddress,ExtensionAttribute2,ExtensionAttribute15,ProxyAddresses,LastLogonDate,PasswordExpired -Server $SearchDCs[$DC]
+                    $DC = $DC + 1
+                } While ($Null -eq $ADUser)
 
-                ForEach ($Dom in $Domain) { 
-                    $ADUser = Get-ADUser -Filter "SamAccountName -Like '$ID*'" -Properties * -Server $Dom
+                If ( $Null -eq $ADuser ) { 'User account not created yet'}
+                ElseIf ($ADUser) {                       
+                    $RegUser.Domain = $SearchDCs[$DC-1].Split('.')[1]
+                    $RegUser.ADAccount = $ADUser.SamAccountName
+                    $RegUser.LastLogonDate = $ADUser.LastLogonDate
+                    $RegUser.PasswordExpired = $ADUser.PasswordExpired
+
+                    $CorrectProxyAddresses = @("SMTP:$($ADUser.EmailAddress)", "smtp:$($ADUser.SamAccountName)@myTUDublin.mail.onmicrosoft.com","sip:$($ADUser.EmailAddress)")
+                    $ProxyTest1 = ($ADUser.ProxyAddresses -contains $CorrectProxyAddresses[0]) -and ($ADUser.ProxyAddresses -contains $CorrectProxyAddresses[1]) -and ($ADUser.ProxyAddresses -contains $CorrectProxyAddresses[2])
+                    $ProxyTest2 = (($ADUser.ProxyAddresses).Count -eq 4) -and ($ADUser.ProxyAddresses -like 'x500*')
+                    $Office365Test = ($ADUser.EmailAddress -eq "$($ADUser.SamAccountName)@mytudublin.ie") -and ($ADUser.ExtensionAttribute15 -eq $ExtensionAttribute15) -and $ProxyTest1 -and $ProxyTest2
+                    $RegUser.'Office365 Valid Setup' = $Office365Test
+
+                    $RegUser.EmailAddress = $ADUser.EmailAddress
                     
-                    If ($ADUser) { 
-                        $FoundDomain.Add($Dom.Split('.')[0])
-                        $DomADAccount.Add($ADUser.SamAccountName)
-                        $DomLastLogonDate.Add($ADUser.LastLogonDate)
-                        $DomPasswordExpired.Add($ADUser.PasswordExpired)
-                        $DomEmailAddress.Add($ADUser.EmailAddress)
-                        $DomDataRetentionPolicy.Add($ADUser.extensionAttribute2)
+                    If ($Null -eq $ADUser.extensionAttribute2){
+                        $RegUser.DataRetentionPolicy = 'N/A'
+                    }
+                    ElseIf ($ADUser.extensionAttribute2){
+                        $RegUser.DataRetentionPolicy = $ADUser.extensionAttribute2 
+                    }        
+    #region ComputingDB Account 
+                    #If Computing, check their DB account
+                    If ($RegUser.Domain -eq 'computing'){
+                        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'DBAccount',([String])))
+                        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'DBAccount_Status',([String])))
+                        $UserTable.Columns.Add((New-Object System.Data.DataColumn 'DBAccount_DefaultPwd',([String])))
+
+                        $CompDBUserTable = New-Object System.Data.DataTable 'CompDB Results'
                         
-                        If ( $FoundDomain | Group-Object | Where-Object Count -gt 1) { 
-                            $RegUser.Duplicate = $FoundDomain        
-                            $Action = $Action + $ActionType.Get_Item('Duplicate')
+                        Get-DBResult  $CompDBconnString $CompDBsqlString $CompDBUserTable
+
+                        If ($CompDBUserTable.Username) { 
+                            $RegUser.DBAccount = $CompDBUserTable.Username
+                            $RegUser.DBAccount_Status = $CompDBUserTable.Account_Status
+                            $RegUser.DBAccount_DefaultPwd = "db$($Reguser.Password)"
+    
+                            $CompDBUserTable.Clear()
                         }
-                        Else { $RegUser.Duplicate = 'No'}
-                    }  
-                }
-
-                If ( $Null -eq $FoundDomain ) { 'User account not created yet'}
-                 
-                # This doesn't work below as the last domain queried comes back blank
-                $RegUser.ADAccount = $DomADAccount
-                $RegUser.LastLogonDate = $DomLastLogonDate
-                $RegUser.PasswordExpired = $DomPasswordExpired
-                $RegUser.EmailAddress = $DomEmailAddress
-                $RegUser.DataRetentionPolicy = $DomDataRetentionPolicy
-                            
-#region ComputingDB Account 
-                            #If Computing, check their DB account
-                            Get-DBResult  $CompDBconnString $CompDBsqlString $CompDBUserTable
-                           
-                            If ($CompDBUserTable.Username) { 
-                                                             $RegUser.DBAccount = $CompDBUserTable.Username
-                                                             $RegUser.DBAccount_Status = $CompDBUserTable.Account_Status
-                                                             $RegUser.DBAccount_DefaultPwd = "db$($Reguser.Password)"
-
-                                                             $CompDBUserTable.Clear()
-                                                           }
-
-                            Else { $CompDBUserTable
-                                   $RegUser.DBAccount = 'N/A'
-                                   $RegUser.DBAccount_Status = 'N/A'
-                                   $RegUser.DBAccount_DefaultPwd = 'N/A'
-                                 }
-#endregion
-
-#region Home Directory
-                            If ($Home) {
-                                        Switch -wildcard ($($RegUser.Programme)) {  
-                                                                                   "TA_K*" { $HomeServer ='comphome' }
-                                                                                   "TA_S*" { $HomeServer = 'scisan' }                                                                 
-                                                                                   "TA_ERA*" { $HomeServer ='comphome' }
-                                                                                   "TA_BDAMKT*" { $HomeServer ='comphome' }
-                                                                                  }
-                                        $HomePath = "\\$HomeServer\HomeDir$\$($RegUser.ADAccount)"
-                                        If (Test-Path $HomePath -PathType Container) {
-                                                                                        $RegUser.HomeFolder = $HomePath
-                                                                                        $Usage =  [math]::Round($(Get-ChildItem $HomePath -Recurse | Measure-Object -Property Length -Sum -ErrorAction SilentlyContinue).Sum /1gb, 2)
-                                                                                        $RegUser.HomeFolderUsage = "$Usage"+ 'GB'
-
-                                                                                        # Check home folder ownership 
-                                                                                        $HomeFolderOwner = (Get-Acl $HomePath).Owner
-                                                                                        $NetBiosName = (Get-ADDomain (($RegUserAD.DistinguishedName.Split(",") | Where-Object {$_ -Like "DC=*"})  -Join ",")).NetBiosName
-                                                                                        # What if more than one domain in ValidUser?
-                                                                                        $NetBiosUser = "$NetBiosName\$($RegUser.ADAccount)"
-                                                                                        If ( $HomeFolderOwner -NE $NetBiosUser) { $RegUser.HomeFolderPermissions = "Error in ownership permissions: Owner is $HomeFolderOwner" }
-                                                                                                                                                                                               
-                                                                                        Else { $RegUser.HomeFolderPermissions = 'Ok' }
-                                                                                                              }
-                                        Else { $RegUser.HomeFolder = "HomeFolder missing on \\$HomeServer\HomeDir$" }    
-                                        }
-                            Else { $RegUser.HomeFolder = "Not requested. Use -Home switch for details" }
-                               
-#endregion                               
-                                                         
+    
+                        Else { $CompDBUserTable
+                            $RegUser.DBAccount = 'No account setup as yet'
+                            $RegUser.DBAccount_Status = 'N/A'
+                            $RegUser.DBAccount_DefaultPwd = 'N/A'
+                        }
+                    }               
+    #endregion                              
+                }                                            
             }
             Else { 
                 "$ID : Not registered"
@@ -525,9 +466,7 @@ We have fixed these errors so that your home folder and files should be accessib
 
         If ($UserTable.Rows[0].Account_Action -eq 'DISABLE') { $Action = $Action + $ActionType.Get_Item('AccountDisabled') }
         If (!$UserTable.Rows[0].PASSWORD -and $UserTable.Rows[0].ID ) { $Action = $Action + $ActionType.Get_Item('NoBirthdate') }
-        If ($Usage -eq $HomeFolderQuota) { $Action = $Action + $ActionType.Get_Item('HomeFolderFull') }
-        If ($RegUser.HomeFolderPermissions -ne 'OK' -and $UserTable.Rows[0].ID -and $HomePath) { $Action = $Action + "`n`n" + $ActionType.Get_Item('HomeFolderPermissions') }
-
+        
         If (!$Action) { $Action = $Action + $ActionType.Get_Item('NoAction') }
 
         $UserTable
@@ -535,9 +474,8 @@ We have fixed these errors so that your home folder and files should be accessib
         Write-Output 'Action:'
         Write-Output $Action
 
-#region CopyToClipboard
+        #CopyToClipboard?
         If ($CopyToClipboard -and $Action) { Set-Clipboard $Action }
-#endregion   
     }
 } # End: Function Get-TUDUser
 
